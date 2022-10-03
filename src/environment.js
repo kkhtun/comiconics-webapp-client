e60220a229ba9bc94b2f6435e5f8c543d321ece1
kkhtun <khaingkhanthtun@gmail.com>
2022-10-03 16:30:01 +0630

    changed host url to render.com

--- src/environment.js
+++ src/environment.js
@@ -2,7 +2,7 @@ const environment = {
     url:
         process.env.REACT_APP_ENV === "local"
             ? "http://localhost:3000"
-            : "https://comiconics-dev.herokuapp.com",
+            : "https://comiconics-server.onrender.com",
     // url: "https://comiconics-dev.herokuapp.com",
 };
 
